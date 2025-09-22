37119ef185e7cec4b5c63a95dae6aad26363ebd6
Seth Millán <millanseth@gmail.com>
2025-09-21 18:44:27 -0600

    feat: :sparkles: mira mama ya hice post jajaja
    hice post jajaja

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,17 @@ import { Elysia } from 'elysia'
 import { node } from '@elysiajs/node'
 
 const app = new Elysia({ adapter: node() })
-	.get('/', () => 'que onda wedo wedito')
+	.get('/', () => 'Pagina principal')
 	.listen(3000, ({ hostname, port }) => {
 		console.log(
 			`🦊 Elysia is running at ${hostname}:${port}`
 		)
 	})
+    .post('/user', ({body}) =>{
+        console.log('Recibimos el dato: ',body)
+        return {
+            message: 'Usuario creado',
+            data: body
+        }
+    })
+    
